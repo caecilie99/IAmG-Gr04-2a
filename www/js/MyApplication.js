f20eb4e717e7e0fb7182736cabb5ceec712b65a3
Martina <martina.schulz@stud.fh-luebeck.de>
2016-05-04 15:49:17 +0200

    bis 4.3 fertig

--- www/js/MyApplication.js
+++ www/js/MyApplication.js
@@ -15,7 +15,7 @@ define(["mwf","mwfUtils","EntityManager","entities","GenericCRUDImplLocal","Gene
 
                 // initialise the local database
                 // TODO-REPEATED: add new entity types to the array of object store names
-                GenericCRUDImplLocal.initialiseDB("mwftutdb", 1, ["MyEntity"], function () {
+                GenericCRUDImplLocal.initialiseDB("mwftutdb", 1, ["MyEntity", "MediaItem"], function () {
 
                     //// TODO-REPEATED: if entity manager is used, register entities and crud operations for the entity types
                     //this.registerEntity("MyEntity", entities.MyEntity, true);
--- www/js/model/MyEntities.js
+++ www/js/model/MyEntities.js
@@ -21,11 +21,55 @@ define(["mwfUtils", "EntityManager"], function (mwfUtils, EntityManager) {
     // use EntityManager.xtends in order to add entity-specific behaviour
     EntityManager.xtends(MyEntity, EntityManager.Entity);
 
+
     // TODO-REPEATED: add new entity type declarations here
+    function MediaItem(name, src, contentType) {
+        // Name
+        this.name = name;
+        // Beschreibung
+        this.description;
+        // Datum
+        this.added = Date.now();
+        // URL
+        this.src = src;
+        // Content Typ
+        this.contentType = contentType;
+        // Bereitstellung des Content
+        this.contentProvision;
+
+        this.instantiateManagedAttributes();
+    }
+    EntityManager.xtends(MediaItem, EntityManager.Entity);
+
+
+    Object.defineProperty(MediaItem.prototype, "addedDateString", {
+        get: function () {
+            return (new Date(this.added)).toLocaleDateString();
+        }
+    });
+
+    Object.defineProperty(MediaItem.prototype, "mediaType", {
+        get: function () {
+            if (this.contentType) {
+                var index = this.contentType.indexOf("/");
+                if (index > -1) {
+                    return this.contentType.substring(0, index);
+                }
+                else {
+                    return "UNKNOWN";
+                }
+            }
+            else {
+                return "UNKNOWN";
+            }
+        }
+    });
+
 
     // TODO-REPEATED: do not forget to export all type declarations
     return {
-        MyEntity: MyEntity
+        MyEntity: MyEntity,
+        MediaItem: MediaItem
     }
 
 });
