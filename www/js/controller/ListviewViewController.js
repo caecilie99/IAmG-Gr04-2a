5ca3ceb767f2b770d3318a8b693ffa3e1ae31c6b
schulzma <martina.schulz@stud.fh-luebeck.de>
2016-05-09 22:22:05 +0200

    4.4.2 Aktionsmenü für Listenelemente fertig, manueller Reload der Seite notwendig

--- www/js/controller/ListviewViewController.js
+++ www/js/controller/ListviewViewController.js
@@ -1,7 +1,7 @@
 /**
  * @author Jörn Kreutel
  */
-define(["mwf","entities", "GenericCRUDImplLocal"], function(mwf, entities, GenericCRUDImplLocal) {
+define(["mwf", "entities", "GenericCRUDImplLocal"], function (mwf, entities, GenericCRUDImplLocal) {
 
     function ListviewViewController() {
         console.log("ListviewViewController()");
@@ -9,9 +9,9 @@ define(["mwf","entities", "GenericCRUDImplLocal"], function(mwf, entities, Gener
         // declare a variable for accessing the prototype object (used für super() calls)
         var proto = ListviewViewController.prototype;
         var items = [
-            new entities.MediaItem("m1","http://lorempixel.com/100/100"),
-            new entities.MediaItem("m2","http://lorempixel.com/200/150"),
-            new entities.MediaItem("m3","http://lorempixel.com/150/200")
+            new entities.MediaItem("m1", "http://lorempixel.com/100/100"),
+            new entities.MediaItem("m2", "http://lorempixel.com/200/150"),
+            new entities.MediaItem("m3", "http://lorempixel.com/150/200")
         ];
         var addNewMediaItemElement;
         var resetDatabaseElement;
@@ -26,37 +26,37 @@ define(["mwf","entities", "GenericCRUDImplLocal"], function(mwf, entities, Gener
             this.initialiseListview(items);
 
             addNewMediaItemElement = this.root.querySelector("#addNewMediaItem");
-            addNewMediaItemElement.onclick = function() {
-                crudops.create(new entities.MediaItem("m","http://lorempixel.com/50/50"), function (created) {
+            addNewMediaItemElement.onclick = function () {
+                crudops.create(new entities.MediaItem("m", "http://lorempixel.com/50/50"), function (created) {
                     this.addToListview(created);
                 }.bind(this));
             }.bind(this);
 
             resetDatabaseElement = this.root.querySelector("#resetDatabase");
-            resetDatabaseElement.onclick = function() {
+            resetDatabaseElement.onclick = function () {
                 if (confirm("Soll die Datenbank wirklich zurückgesetzt werden?")) {
-                        indexedDB.deleteDatabase("mwftutdb");
+                    indexedDB.deleteDatabase("mwftutdb");
                 }
             }.bind(this);
 
-            crudops.readAll(function(items){
+            crudops.readAll(function (items) {
                 this.initialiseListview(items);
             }.bind(this));
 
             // call the superclass once creation is done
-            proto.oncreate.call(this,callback);
+            proto.oncreate.call(this, callback);
         }
 
         /*
          * for views with listviews: bind a list item to an item view
          * TODO: delete if no listview is used or if databinding uses ractive templates
          */
- 
+
         /*
          * for views with listviews: react to the selection of a listitem
          * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
          */
-        this.onListItemSelected = function(listitem,listview) {
+        this.onListItemSelected = function (listitem, listview) {
             // TODO: implement how selection of listitem shall be handled
             alert("Element " + listitem.name + listitem._id + " wurde ausgewählt!");
         }
@@ -65,25 +65,36 @@ define(["mwf","entities", "GenericCRUDImplLocal"], function(mwf, entities, Gener
          * for views with listviews: react to the selection of a listitem menu option
          * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
          */
-        this.onListItemMenuItemSelected = function(option, listitem, listview) {
+        this.onListItemMenuItemSelected = function (option, listitem, listview) {
             // TODO: implement how selection of option for listitem shall be handled
+            proto.onListItemMenuItemSelected.call(this, option, listitem, listview);
         }
 
         /*
          * for views with dialogs
          * TODO: delete if no dialogs are used or if generic controller for dialogs is employed
          */
-        this.bindDialog = function(dialogid,dialog,item) {
+        this.bindDialog = function (dialogid, dialog, item) {
             // call the supertype function
-            proto.bindDialog.call(this,dialogid,dialog,item);
+            proto.bindDialog.call(this, dialogid, dialog, item);
             // TODO: implement action bindings for dialog, accessing dialog.root
         }
-
+        this.deleteItem = function (item) {
+            crudops.delete(item._id, function () {
+                this.removeFromListview(item.id);
+            }.bind(this));
+        }
+        this.editItem = function (item) {
+            item.name = (item.name + item.name);
+            crudops.update(item._id, item, function () {
+                this.updateInListview(item.id, item);
+            }.bind(this));
+        }
 
     }
 
     // extend the view controller supertype
-    mwf.xtends(ListviewViewController,mwf.ViewController);
+    mwf.xtends(ListviewViewController, mwf.ViewController);
 
     // and return the view controller function
     return ListviewViewController;
